9c39d79aa1ad6de00e7089b6a1342d507eb2960b
academic234 <167694886+academic234@users.noreply.github.com>
2024-04-21 21:00:58 +0530

    Streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,12 @@
 import streamlit as st
-st.write("Hello World youu")
+def main():
+  st.title("Find The Largest Number")
+  num1 = st.number_input("Enter the first number:", step=1)
+  num2 = st.number_input("Enter the second number:", step=1)
+  num3 = st.number_input("Enter the third number:", step=1)
+  if st.button("Find The Largest"):
+    largest = max(num1,num2,num3)
+    st.write("The Largest Number is: ",largest)
+if __name__ == "__main__":
+  main()
+
